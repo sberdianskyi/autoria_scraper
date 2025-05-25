d114b2164d0aa8eb9858969a674efd246c19457a
Serhii Berdianskyi <zeferot94@gmail.com>
2025-05-26 00:23:14 +0300

    added items.py for scraper

---
+++ autoria_scraper/items.py
@@ -0,0 +1,21 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+from datetime import datetime
+
+import scrapy
+
+
+class AutoriaScraperItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    price_usd = scrapy.Field()
+    odometer = scrapy.Field()
+    username = scrapy.Field()
+    phone_number = scrapy.Field()
+    image_url = scrapy.Field()
+    images_count = scrapy.Field()
+    car_number = scrapy.Field()
+    car_vin = scrapy.Field()
+    datetime_found = scrapy.Field()
